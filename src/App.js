1cacc83504f5c8d4c4adb8e37daf38a3f8358c8c
idanb93 <idanbar93@gmail.com>
2022-06-19 17:52:20 +0300

    containers and divisions

--- src/App.js
+++ src/App.js
@@ -15,19 +15,120 @@ function App() {
 
       <BrowserRouter>
         <Navbar />
-        <header className="App-header">
-          <Routes>
-            <Route path='/contact' element={<Contact />} />
-          </Routes>
-        </header>
-
+        <Routes>
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
       </BrowserRouter>
 
       <div id='intro'>
-        <div>
-          <h2 style={{ color: 'white' }}>Hi My name is Idan Bardugo and I am an experienced Full-Stack Developer</h2>
+        <h2 style={{ color: 'whitesmoke' }}>Hi My name is Idan Bardugo and I am an experienced Full-Stack Developer</h2>
+        <div id='intro-picture-container'>
+          <img id='intro-picture' src={require('./img/idan2.jpeg')} />
         </div>
       </div>
+
+      <div id='about-me'>
+
+        <div id='about-me-title'>
+          <h2>Hi, I'm Idan</h2>
+        </div>
+
+        <div id='about-me-body'>
+          <p>In the past 2 years i worked at Cato Networks I'm passionate about cyber security</p>
+        </div>
+
+      </div>
+
+      <div id='cards-conatiner'>
+        <div id='cards'>
+
+          <div className='card'>
+            <div className='card-icon'></div>
+            <div className='card-title'>
+              <h2>Front-End Developer</h2></div>
+            <div className='card-body'>
+              <p>Building user interfaces from scratch.
+                Using various state management like Redux, Vuex, Mobx.
+                Experience in building complex UI systems with huge scale of data.</p>
+            </div>
+            <div className='card-technologies'>
+              <h4>Front-end Frameworks and Tools:</h4>
+                <li className='tool'>React.js</li>
+                <li className='tool'>Redux</li>
+                <li className='tool'>Bootstrap</li>
+                <li className='tool'>Matrial UI</li>
+            </div>
+          </div>
+
+          <div className='card'>
+            <div className='card-icon'></div>
+            <div className='card-title'>
+              <h2>Back-End Developer</h2></div>
+            <div className='card-body'>
+              <p>Building user interfaces from scratch.
+                Using various state management like Redux, Vuex, Mobx.
+                Experience in building complex UI systems with huge scale of data.</p>
+            </div>
+            <div className='card-technologies'>
+              <h4>Front-end Frameworks and Tools:</h4>
+                <li className='tool'>Node.js</li>
+                <li className='tool'>Express.js</li>
+                <li className='tool'>Express Router</li>
+                <li className='tool'>JWT</li>
+                <li className='tool'>Python</li>
+                <li className='tool'>Sub-Processes</li>
+                <li className='tool'>Java</li>
+                <li className='tool'>Multi-Threaded Programming</li>
+            </div>
+          </div>
+
+          <div className='card'>
+            <div className='card-icon'></div>
+            <div className='card-title'>
+              <h2>Cloud</h2></div>
+            <div className='card-body'>
+              <p>Building user interfaces from scratch.
+                Using various state management like Redux, Vuex, Mobx.
+                Experience in building complex UI systems with huge scale of data.</p>
+            </div>
+            <div className='card-technologies'>
+              <h4>Front-end Frameworks and Tools:</h4>
+                <li className='tool'>React.js</li>
+                <li className='tool'>Redux</li>
+                <li className='tool'>Bootstrap</li>
+                <li className='tool'>Matrial UI</li>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+      <div id='projects'>
+        <p>Here are some of my projects</p>
+        <div id='projects-container'>
+          <div className='project'>
+            <a href="https://www.loom.com/embed/f7294c49890e492a86f51121d3ad4b85">
+              <img className='project-image' src="https://cdn.loom.com/sessions/thumbnails/f7294c49890e492a86f51121d3ad4b85-with-play.gif" />
+            </a>
+
+          </div>
+          <div className='project'></div>
+          <div className='project'></div>
+          <div className='project'></div>
+          <div className='project'></div>
+          <div className='project'></div>
+        </div>
+      </div>
+
+      <footer>
+        <div><p style={{color: 'whitesmoke'}}>With Great Power Comes Great Responsibility</p></div>
+        <div id='footer-button-container'>
+          <button className='footer-button'>Github</button>
+          <button className='footer-button'>Linkedin</button>
+          <button className='footer-button'>Email</button>
+        </div>
+      </footer>
 
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,10 +3,12 @@ import { Link } from "react-router-dom";
 function Navbar() {
 
     return (
-        <div className="navbar">
-            <Link className="navbar-item " to={'/contact'}>Contact</Link>
-            <Link className="navbar-item " to={'/cv'}>CV</Link>
-        </div>
+        <>
+            <div className="navbar">
+                <Link className="navbar-item " to={'/contact'}>Contact</Link>
+                <Link className="navbar-item " to={'/cv'}>CV</Link>
+            </div>
+        </>
     )
 
 }
