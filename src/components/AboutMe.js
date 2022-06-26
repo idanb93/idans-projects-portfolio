73200a19be8d759ac874d414663718ed98db22fe
idanb93 <idanbar93@gmail.com>
2022-06-26 13:56:43 +0300

    change the s3 bucket used for deployment

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -8,7 +8,7 @@ function AboutMe() {
             </div>
 
             <div id='about-me-body'>
-                <h3>Full Stack Developer experienced in JavaScript and Python based programming with a background in Cyber.</h3>
+                <h3>Full Stack Developer experienced in JavaScript and Python based programming with a background in Machine Learning.</h3>
                 <h3>Possess strong skills in computer networks and troubleshooting that help cyber companies deliver features and fix bugs more quickly</h3>
                 <h3>I have rich experience with a variety of programming languages ​​and frameworks, development of E2E features from scratch on both server side and client side. </h3>
                 <h3>problem solver, independent employee with high attention to detail. Fan of MMA, outdoor activities, TV series and English literature </h3>
