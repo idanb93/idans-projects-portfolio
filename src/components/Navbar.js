376e1441981d2e5e02a1596c7f3ca4d9b33a24a0
idanb93 <idanbar93@gmail.com>
2022-10-24 23:05:12 +0300

    CV button is now downloading the CV from the S3 bucket vs the previously configured Linkedin CDN

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,69 +1,64 @@
-import { Link } from "react-router-dom";
-import { useLocation } from 'react-router-dom';
+import { Link } from "react-router-dom"
+import { useLocation } from "react-router-dom"
 
 function Navbar() {
+  const location = useLocation()
 
-    const location = useLocation();
+  const openRoutes = ["/"]
 
-    const openRoutes = [
-        '/',
-    ];
+  const isMainPage = openRoutes.includes(location.pathname)
 
-    const isMainPage = openRoutes.includes(location.pathname);
-
-    return (
-        <div>
-
-            {isMainPage &&
-                <div className="navbar">
-                    <div className="navbar-item ">
-                        <Link to={'/contact'}>
-                            <p className="navbar-text">Contact</p>
-                        </Link>
-                    </div>
-                    <div className="navbar-item ">
-                        <a href="https://media-exp1.licdn.com/dms/document/C4D2DAQFkdR1leemrSg/profile-treasury-document-pdf-analyzed/0/1655800588755?e=1656547200&v=beta&t=NoUVa-irrcJTuSlVNJ_bHkF0-62C4g-Qpwgofl7dcPI">
-                            <p className="navbar-text">CV</p>
-                        </a>
-                    </div>
-                </div>
-            }
-
-            {!isMainPage &&
-                <div className="navbar">
-                    <div className="navbar-item ">
-                        <Link to={'/'}>
-                            <p className="navbar-text">Main Page</p>
-                        </Link>
-                    </div>
-                    <div className="navbar-item ">
-                    <a href="https://media-exp1.licdn.com/dms/document/C4D2DAQFkdR1leemrSg/profile-treasury-document-pdf-analyzed/0/1655800588755?e=1656547200&v=beta&t=NoUVa-irrcJTuSlVNJ_bHkF0-62C4g-Qpwgofl7dcPI">
-                            <p className="navbar-text">CV</p>
-                        </a>
-                    </div>
-                </div>
-            }
+  return (
+    <div>
+      {isMainPage && (
+        <div className="navbar">
+          <div className="navbar-item ">
+            <Link to={"/contact"}>
+              <p className="navbar-text">Contact</p>
+            </Link>
+          </div>
+          <div className="navbar-item ">
+            <a href="https://s3.amazonaws.com/www.idans-portfolio.com/Idan's+Resume.pdf">
+              <p className="navbar-text">CV</p>
+            </a>
+          </div>
         </div>
-    )
+      )}
 
-    return (
+      {!isMainPage && (
+        <div className="navbar">
+          <div className="navbar-item ">
+            <Link to={"/"}>
+              <p className="navbar-text">Main Page</p>
+            </Link>
+          </div>
+          <div className="navbar-item ">
+            <a href="https://s3.amazonaws.com/www.idans-portfolio.com/Idan's+Resume.pdf">
+              <p className="navbar-text">CV</p>
+            </a>
+          </div>
+        </div>
+      )}
+    </div>
+  )
 
-        <>
-            <div className="navbar">
-                <div className="navbar-item ">
-                    <Link to={'/contact'}>
-                        <p className="navbar-text">Contact</p>
-                    </Link>
-                </div>
-                <div className="navbar-item ">
-                    <Link to={'/cv'}>
-                        <p className="navbar-text">CV</p>
-                    </Link>
-                </div>
-            </div>
-        </>
-    )
+  // return (
 
+  //     <>
+  //         <div className="navbar">
+  //             <div className="navbar-item ">
+  //                 <Link to={'/contact'}>
+  //                     <p className="navbar-text">Contact</p>
+  //                 </Link>
+  //             </div>
+  //             <div className="navbar-item ">
+  //                 <Link to={'/cv'}>
+  //                     <p className="navbar-text">CV</p>
+  //                 </Link>
+  //             </div>
+  //         </div>
+  //     </>
+  // )
 }
 
-export default Navbar;
+export default Navbar
